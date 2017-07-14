da36859d553988e969341d50d297e683bf8c768d
刘畅 <liuchang@s-MacBook-Pro.local>
2017-07-14 09:57:06 +0800

    Completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,24 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Chang Liu
  */
 public class LeapYear {
+
+    /** Checks whether the input YEAR is a leap year*/
+    public static boolean isLeapYear(int year) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (year % 4 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
