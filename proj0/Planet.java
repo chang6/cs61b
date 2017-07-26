84629802d17d2bb6ebf58d3ca3b0ffc790c8dc37
刘畅 <liuchang@s-MacBook-Pro.local>
2017-07-26 09:30:46 -0600

    Completed constructor of the Planet class

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,29 @@
 public class Planet {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Planet(double xP, double yP, double xV,
+              double yV, double m, String img) {
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+
+	public Planet(Planet p) {		
+		this.xxPos = p.xxPos;
+		this.yyPos = p.yyPos;
+		this.xxVel = p.xxVel;
+		this.yyVel = p.yyVel;
+		this.mass = p.mass;
+		this.imgFileName = p.imgFileName;
+	}
+
+	
 }
