96bddef86007c159ef7e7d2c3654835291d0c182
刘畅 <liuchang@s-MacBook-Pro.local>
2017-07-26 17:02:49 -0600

    Completed Planet class

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -34,5 +34,54 @@ public class Planet {
         return distance;
 	}
 
+	public double calcForceExertedBy(Planet p) {
+		double gravConst = 6.67 * Math.pow(10, -11);
+		double distance = this.calcDistance(p);
+		double f = gravConst * this.mass * p.mass / (distance * distance);
+
+		return f;
+	}
+
+	public double calcForceExertedByX(Planet p) {
+		double rate = (p.xxPos - this.xxPos) / this.calcDistance(p);
+		double fX = this.calcForceExertedBy(p) * rate;
+
+		return fX;
+	}
+
+	public double calcForceExertedByY(Planet p) {
+		double rate = (p.yyPos - this.yyPos) / this.calcDistance(p);
+		double fY = this.calcForceExertedBy(p) * rate;
+
+		return fY;
+	}
+
+	public double calcNetForceExertedByX(Planet[] allPlanets) {
+		double fNetX = 0;
+		for (Planet p : allPlanets) {
+			if (!this.equals(p)) {
+				fNetX = fNetX + this.calcForceExertedByX(p);
+			}
+		}
+		return fNetX;
+	}
+
+	public double calcNetForceExertedByY(Planet[] allPlanets) {
+		double fNetY = 0;
+		for (Planet p : allPlanets) {
+			if (!this.equals(p)) {
+				fNetY = fNetY + this.calcForceExertedByY(p);
+			}
+		}
+		return fNetY;
+	}
 	
+	public void update(double dt, double fX, double fY) {
+		double aX = fX / this.mass;
+		double aY = fY / this.mass;
+		this.xxVel = this.xxVel + dt * aX;
+		this.yyVel = this.yyVel + dt * aY;
+		this.xxPos = this.xxPos + this.xxVel * dt;
+		this.yyPos = this.yyPos + this.yyVel * dt;
+	}
 }
